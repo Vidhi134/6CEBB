690d72e9373a0a824538540b017217815058d010
Vidhi134 <vidhikathiriya509@gmail.com>
2024-12-24 10:09:48 +0530

    Calculator Activity

---
+++ 6CEB/Calculator.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Calculator.aspx.cs" Inherits="_6CEB.Calculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Calculator</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Number 1: "></asp:Label>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Label ID="Label2" runat="server" Text="Number 2: "></asp:Label>
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Label ID="Label3" runat="server" Text="Operation: "></asp:Label>
+            <asp:DropDownList ID="DropDownList1" runat="server">
+                <asp:ListItem Text="Add" Value="Add"></asp:ListItem>
+                <asp:ListItem Text="Subtract" Value="Subtract"></asp:ListItem>
+                <asp:ListItem Text="Multiply" Value="Multiply"></asp:ListItem>
+                <asp:ListItem Text="Divide" Value="Divide"></asp:ListItem>
+            </asp:DropDownList>
+        </div>
+        <div>
+            <asp:Button ID="Button1" runat="server" Text="Calculate" OnClick="Button1_Click" />
+        </div>
+        <div>
+            <asp:Label ID="ResultLabel" runat="server" Text="Result: " Font-Bold="True"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 6CEB/Calculator.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _6CEB
+{
+    public partial class Calculator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+                
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Get the input numbers
+                double num1 = Convert.ToDouble(TextBox1.Text);
+                double num2 = Convert.ToDouble(TextBox2.Text);
+                string operation = DropDownList1.SelectedValue;
+                double result = 0;
+
+                // Perform the selected operation
+                switch (operation)
+                {
+                    case "Add":
+                        result = num1 + num2;
+                        break;
+                    case "Subtract":
+                        result = num1 - num2;
+                        break;
+                    case "Multiply":
+                        result = num1 * num2;
+                        break;
+                    case "Divide":
+                        if (num2 != 0)
+                        {
+                            result = num1 / num2;
+                        }
+                        else
+                        {
+                            ResultLabel.Text = "Result: Division by zero is not allowed.";
+                            return;
+                        }
+                        break;
+                }
+
+                // Display the result
+                ResultLabel.Text = "Result: " + result.ToString();
+            }
+            catch (Exception ex)
+            {
+                ResultLabel.Text = "Error: " + ex.Message;
+            }
+        }
+    }
+}
+    
